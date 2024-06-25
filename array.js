0ac16965c1355f127a4d5cbabaadca2397129845
X-workz <xworkzodc@gmail.com>
2024-06-25 16:37:54 +0530

    new files

---
+++ array.js
@@ -0,0 +1,20 @@
+var Java =[5,6,10,50];
+console.log(Java);
+
+var afteraddelement = Java.push(25);
+console.log(afteraddelement);
+console.log(Java);
+
+var popElement = Java.pop();
+console.log(popElement);
+console.log(Java);
+
+Java.unshift(69);
+console.log(Java);
+
+var shiftmethod = Java.shift();
+console.log(shiftmethod);
+console.log(Java);
+
+Java.splice(2,0,66,78);
+console.log(Java);
---
+++ math.js
@@ -0,0 +1,5 @@
+function sum(){
+    var firstnumber=document.getElementById("firstnum").value;
+    var secondnumber=document.getElementById("secondnum").value;
+    console.log(Number(firstnumber) + Number(secondnumber));
+}
